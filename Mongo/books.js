a877f1fa218c5dc1c7e99e97e3a096f5cf1d8a74
taslima864 <sheikhtaslima00@gmail.com>
2025-12-11 20:52:12 +0530

    Learned how to validate or apply rules in schema

---
+++ Mongo/books.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+main()
+  .then(() => {
+    console.log("connection successfull");
+  })
+  .catch(() => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/amazon");
+}
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+let book1 = new Book({
+  title: "Mathematics XII",
+  author: "RD Sharma",
+  price: 1200,
+});
+
+book1
+  .save()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
