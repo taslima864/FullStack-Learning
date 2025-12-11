920ca2ed2a3c05352a2effe6b0e2df2e19db74d8
taslima864 <sheikhtaslima00@gmail.com>
2025-12-11 20:06:55 +0530

    Learn all the CRUD operations

--- Mongo/index.js
+++ Mongo/index.js
@@ -18,10 +18,27 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-User.findOneAndUpdate({name: "Will"}, {age: 25}, {new: true})
-.then((res) => {
-  console.log(res);
-}).catch((err) =>{
-  console.log(err);
-});
+User.deleteOne({ name: "John" })
+  .then((res) => {
+    console.log(res); 
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+
+
+User.findByIdAndDelete("6939838e3e0ac31c57bc0471")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// User.findOneAndUpdate({name: "Will"}, {age: 25}, {new: true})
+// .then((res) => {
+//   console.log(res);
+// }).catch((err) =>{
+//   console.log(err);
+// });
