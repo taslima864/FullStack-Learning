5379d9a6966c3da79c54aa89329ea547ee4b587f
taslima864 <sheikhtaslima00@gmail.com>
2025-12-13 02:48:58 +0530

    Learned how to initialize database

---
+++ Mongo3/init.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const chats = require("./models/chats.js");
+
+main()
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+let allChats = [
+  {
+    from: "neha",
+    to: "priya",
+    msg: "Hi!!!",
+    created_at: new Date(),
+  },
+
+  {
+    from: "kaif",
+    to: "saif",
+    msg: "teach me js",
+    created_at: new Date(),
+  },
+
+  {
+    from: "mohit",
+    to: "rohit",
+    msg: "Good luck",
+    created_at: new Date(),
+  },
+
+  {
+    from: "Taslima",
+    to: "Priyanka",
+    msg: "Go to heaven!!!",
+    created_at: new Date(),
+  },
+];
+
+chats.insertMany(allChats);
