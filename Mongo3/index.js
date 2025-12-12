f4ce6f1ed9276e716d014c6becb0b765663e10e9
taslima864 <sheikhtaslima00@gmail.com>
2025-12-13 01:37:38 +0530

    Set up MongoDB with express

---
+++ Mongo3/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+main()
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+
+
+app.get("/", (req, res) => {
+  res.send("root is working");
+});
+
+app.listen(8080, () => {
+  console.log(`Server is listening on port 8080`);
+});
