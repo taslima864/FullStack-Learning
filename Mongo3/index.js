cb8fc3a7d848e0e44bda13e0624d5a19284ed37a
taslima864 <sheikhtaslima00@gmail.com>
2025-12-13 02:24:35 +0530

    Creating Chat model

--- Mongo3/index.js
+++ Mongo3/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-
+const chats = require("./models/chats.js");
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -17,7 +17,16 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
+let chat1 = new chats({
+  from: "neha",
+  to: "priya",
+  msg: "Hi!!!",
+  created_at: new Date(),
+});
 
+chat1.save().then((res) => {
+  console.log(res);
+})
 
 app.get("/", (req, res) => {
   res.send("root is working");
