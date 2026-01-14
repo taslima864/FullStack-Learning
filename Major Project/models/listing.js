e5e4d2d8226d15ce9d4903eb5b40e8c7faca84ba
taslima864 <sheikhtaslima00@gmail.com>
2026-01-14 23:50:26 +0530

    Handling: Delete Listing

--- Major Project/models/listing.js
+++ Major Project/models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 const listingSchema = new Schema({
   title: { type: String, required: true },
@@ -37,6 +38,12 @@ const listingSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
