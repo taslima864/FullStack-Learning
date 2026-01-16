b3ccfbe80da6c3b1fa0f47bb9ae1d8d5853d0f13
taslima864 <sheikhtaslima00@gmail.com>
2026-01-16 23:20:51 +0530

    Restructing Reviews

---
+++ Major Project/routers/review.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+
+const Listing = require("../models/listing");
+const Review = require("../models/review");
+const wrapAsync = require("../utils/wrapAsync");
+const ExpressError = require("../utils/ExpressError");
+const { reviewSchema } = require("../schema");
+
+// validation middleware
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(",");
+    throw new ExpressError(400, msg);
+  }
+  next();
+};
+
+// CREATE review
+router.post(
+  "/",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    const listing = await Listing.findById(req.params.id);
+    const review = new Review(req.body.review);
+
+    listing.reviews.push(review);
+
+    await review.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+  })
+);
+
+// DELETE review
+router.delete(
+  "/:reviewId",
+  wrapAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+module.exports = router;
