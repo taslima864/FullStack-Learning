f6b664237151aa925d3634fad3d26cefe842b34c
taslima864 <sheikhtaslima00@gmail.com>
2026-01-17 21:17:47 +0530

    Storing & Using Session Info

--- Major Project/classroom/server.js
+++ Major Project/classroom/server.js
@@ -2,23 +2,33 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 
-app.use(
-  session({
-    secret: "mysupersecretstring",
-    resave: false,
-    saveUninitialized: true,
-  }),
-);
+const sessionOptions = {
+  secret: "mysupersecretstring",
+  resave: false,
+  saveUninitialized: true,
+};
+app.use(session(sessionOptions));
 
-app.get("/reqcount", (req, res) => {
-  if (req.session.count) {
-    req.session.count++;
-  } else {
-    req.session.count = 1;
-  }
-
-  res.send(`You sent a request ${req.session.count} times`);
+app.get("/register", (req, res) => {
+  let { name = "anonymous" } = req.query;
+  req.session.name = name;
+  console.log(req.session.name);
+  res.redirect("/hello");
 });
+
+app.get("/hello", (req, res) => {
+  res.send(`hello, ${req.session.name}`);
+});
+
+// app.get("/reqcount", (req, res) => {
+//   if (req.session.count) {
+//     req.session.count++;
+//   } else {
+//     req.session.count = 1;
+//   }
+
+//   res.send(`You sent a request ${req.session.count} times`);
+// });
 
 // app.get("/test", (req, res) => {
 //   res.send("test successful!");
