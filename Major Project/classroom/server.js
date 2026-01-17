9305ea24e1b9719981b756dd25f4e4448d26e8b9
taslima864 <sheikhtaslima00@gmail.com>
2026-01-18 01:06:29 +0530

    Using connect-flash

--- Major Project/classroom/server.js
+++ Major Project/classroom/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const sessionOptions = {
   secret: "mysupersecretstring",
@@ -8,16 +13,17 @@ const sessionOptions = {
   saveUninitialized: true,
 };
 app.use(session(sessionOptions));
+app.use(flash());
 
 app.get("/register", (req, res) => {
   let { name = "anonymous" } = req.query;
   req.session.name = name;
-  console.log(req.session.name);
+  req.flash("success", "user registered successfully!!");
   res.redirect("/hello");
 });
 
 app.get("/hello", (req, res) => {
-  res.send(`hello, ${req.session.name}`);
+  res.render("page.ejs", { name: req.session.name, msg: req.flash("success") });
 });
 
 // app.get("/reqcount", (req, res) => {
