b778272fa305d155822815a6d5e261cae897f230
taslima864 <sheikhtaslima00@gmail.com>
2026-01-24 02:17:58 +0530

    Login User

--- Major Project/routes/user.js
+++ Major Project/routes/user.js
@@ -1,8 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+const User = require("../models/user");
 
+// SIGNUP FORM
 router.get("/signup", (req, res) => {
-  res.render("users/signup.ejs");
+  res.render("users/signup");
 });
+
+// SIGNUP LOGIC
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+
+      req.flash("success", "Welcome to Wanderlust!");
+      res.redirect("/login");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+// LOGIN FORM
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome back!");
+    res.redirect("/listings");
+  }
+);
 
 module.exports = router;
