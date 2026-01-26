1dfe8ddc42fb884fc0490748624e12f39f6dafcc
taslima864 <sheikhtaslima00@gmail.com>
2026-01-26 21:24:37 +0530

    Login after Signup

--- Major Project/routes/user.js
+++ Major Project/routes/user.js
@@ -17,9 +17,14 @@ router.post(
       const { username, email, password } = req.body;
       const newUser = new User({ email, username });
       const registeredUser = await User.register(newUser, password);
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        }
 
-      req.flash("success", "Welcome to Wanderlust!");
-      res.redirect("/login");
+        req.flash("success", "Welcome to Wanderlust!");
+        res.redirect("/listings");
+      });
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("/signup");
@@ -44,6 +49,7 @@ router.post(
   },
 );
 
+// LOGOUT ROUTE
 router.get("/logout", (req, res, next) => {
   req.logout((err) => {
     if (err) {
