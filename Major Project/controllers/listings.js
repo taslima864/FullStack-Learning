0b9729988ddefa282a892c3252ae25c809fb1345
taslima864 <sheikhtaslima00@gmail.com>
2026-02-05 23:12:25 +0530

    Display Image

--- Major Project/controllers/listings.js
+++ Major Project/controllers/listings.js
@@ -35,10 +35,10 @@ module.exports.showListing = async (req, res) => {
 module.exports.createListing = async (req, res,next) => {
   let url = req.file.path;
   let filename = req.file.filename;
-  // const newListing = new Listing(req.body.listing);
-  // newListing.owner = req.user._id;
-  // newListing.image = {url, filename};
-  // await newListing.save();
+
+  const newListing = new Listing(req.body.listing);
+  newListing.owner = req.user._id;
+  await newListing.save();
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
 };
@@ -70,3 +70,4 @@ module.exports.destroyListing = async (req, res) => {
     req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
   };
+
