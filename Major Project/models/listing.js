ebb2e00de8ad3dba42c9645110d51f7730a2dbc0
taslima864 <sheikhtaslima00@gmail.com>
2026-02-07 22:27:07 +0530

    Storing Coordinates

--- Major Project/models/listing.js
+++ Major Project/models/listing.js
@@ -1,63 +1,41 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Review = require("./review");
 
 const listingSchema = new Schema({
-  title: { type: String, required: true },
+  title: String,
   description: String,
-  type: String,
+  price: Number,
+  location: String,
+  country: String,
+
+  geometry: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  },
 
   image: {
-    filename: {
-      type: String,
-      default: "listingimage",
-    },
-    url: {
-      type: String,
-      default: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b",
-      set: (v) =>
-        v === ""
-          ? "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b"
-          : v,
-    },
+    url: String,
+    filename: String
   },
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
+
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
   },
-  location: {
-    type: String,
-  },
-  country: {
-    type: String,
-  },
+
   reviews: [
     {
       type: Schema.Types.ObjectId,
-      ref: "Review",
-    },
-  ],
-  owner:{
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-
-  geometry: {
-  lat: Number,
-  lng: Number
-}
-
-
+      ref: "Review"
+    }
+  ]
 });
 
-
-
-listingSchema.post("findOneAndDelete", async (listing) => {
-  if (listing) {
-    await Review.deleteMany({ _id: { $in: listing.reviews } });
-  }
-});
-
-const Listing = mongoose.model("Listing", listingSchema);
-module.exports = Listing;
+module.exports = mongoose.model("Listing", listingSchema);
