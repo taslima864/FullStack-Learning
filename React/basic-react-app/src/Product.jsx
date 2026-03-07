a9731e82a91892f5c258161d072dcc53bd9f74ea
taslima864 <sheikhtaslima00@gmail.com>
2026-03-07 08:11:32 +0530

    Dynamic Component Styling

--- React/basic-react-app/src/Product.jsx
+++ React/basic-react-app/src/Product.jsx
@@ -1,14 +1,15 @@
 import "./Product.css";
 
 function Product({ title, price, features }) {
-    return (
-      <div className="Product">
-        <h3>{title}</h3>
-        <h5>Price: {price}</h5>
-        { price > 30000 && <p>Discount of 5%</p>}
-      </div>
-    );
-  
+  let isDiscount = price > 30000;
+  let styles = { backgroundColor: isDiscount ? "yellow" : "" };
+  return (
+    <div className="Product" style={styles}>
+      <h3>{title}</h3>
+      <h5>Price: {price}</h5>
+      {isDiscount && <p>Discount of 5%</p>}
+    </div>
+  );
 }
 
 export default Product;
