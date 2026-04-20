3ac7780ae971361fbcaadb98ec5691ecd897e644
taslima864 <sheikhtaslima00@gmail.com>
2026-04-20 17:48:41 +0530

    Handling Non-Click Events

--- React/basic-react-app/src/Button.jsx
+++ React/basic-react-app/src/Button.jsx
@@ -1,16 +1,25 @@
-function printHello() {
+function handleClick() {
   console.log("hello");
-  }
+}
 
-  function printBye(){
-    console.log("Bye!!");
-  }
-  export default function Button() {
-    return (
-      <div>
-        <button onClick={printHello}>Click me!</button>
-        <p onClick={printBye}>this para is event demo</p>
-      </div>
-    );  
-  }
+function handleMouseOver() {
+  console.log("Bye!!");
+}
 
+function handleDblClick() {
+  console.log("You doubled Clicked");
+}
+export default function Button() {
+  return (
+    <div>
+      <button onClick={handleClick}>Click me!</button>
+      <p onMouseOver={handleMouseOver}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        dolor sunt odio iusto sint, vel dolores fugiat quos quibusdam, nam
+        itaque voluptates? Provident delectus laudantium nemo, aliquam
+        perferendis cum perspiciatis!
+      </p>
+      <button onDoubleClick={handleDblClick}>double click me!!</button>
+    </div>
+  );
+}
