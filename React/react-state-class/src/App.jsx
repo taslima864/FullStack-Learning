889176953ce7418be83e4482ba4dcac2446731e5
taslima864 <sheikhtaslima00@gmail.com>
2026-04-21 12:24:31 +0530

    State in React

---
+++ React/react-state-class/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Counter from "./counter";
+
+function App() {
+  return (
+    <>
+      <h1>States in React</h1>
+      <Counter/>
+    </>
+  );
+}
+
+export default App;
