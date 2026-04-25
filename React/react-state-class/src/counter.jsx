04136d29aacae03f2672ec61d31888c2578e42b8
taslima864 <sheikhtaslima00@gmail.com>
2026-04-25 23:40:14 +0530

    More about state

--- React/react-state-class/src/counter.jsx
+++ React/react-state-class/src/counter.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 
+function init() {
+  console.log("Init was executed");
+  return Math.random();
+}
+
 export default function Counter() {
-  let [count, setCount] = useState(0); //Initialization
+  let [count, setCount] = useState(init()); //Initialization
+  console.log("Component was re-rendered");
 
   let incCount = () => {
     setCount((currentCount) => {
       return currentCount + 1;
     });
-    setCount((currentCount) => {
-      return currentCount + 1;
-    });
+  
   };
 
   return (
