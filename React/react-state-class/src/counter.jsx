3a7906ed71432bdd9f40241963e4735c74c1e355
taslima864 <sheikhtaslima00@gmail.com>
2026-04-25 22:32:02 +0530

    re-render

--- React/react-state-class/src/counter.jsx
+++ React/react-state-class/src/counter.jsx
@@ -2,8 +2,12 @@ import { useState } from "react";
 
 export default function Counter() {
   let [count, setCount] = useState(0);
+  console.log("Counter component rendered");
+  console.log(`Current count: ${count}`);
 
+  
   let incCount = () => {
+    console.log(`Inside count: ${count}`);
     setCount(count + 1);
   };
 
