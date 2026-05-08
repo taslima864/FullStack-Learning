3d742ef06fdc844bc1b99df36411916015ab349a
taslima864 <sheikhtaslima00@gmail.com>
2026-05-08 16:18:44 +0530

    Task done by checkbox implmentation

--- React/react-state-class/src/TodoList.jsx
+++ React/react-state-class/src/TodoList.jsx
@@ -2,37 +2,89 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList() {
-  let [todos, setTodos] = useState([{ task: "sample-task", id: uuidv4() }]);
+  let [todos, setTodos] = useState([
+    {
+      task: "sample-task",
+      id: uuidv4(),
+      done: false,
+    },
+  ]);
 
   let [newTodo, setNewTodo] = useState("");
 
+  // Add New Task
   let addNewTask = () => {
+    if (newTodo.trim() === "") return;
+
     setTodos((prevTodos) => {
-      return [...prevTodos, { task: newTodo, id: uuidv4() }];
+      return [
+        ...prevTodos,
+        {
+          task: newTodo,
+          id: uuidv4(),
+          done: false,
+        },
+      ];
     });
 
     setNewTodo("");
   };
 
+  // Update Input Value
   let updateTodoValue = (event) => {
     setNewTodo(event.target.value);
   };
-let deleteTodo = (id) => {
-  setTodos((prevTodos) =>
-    prevTodos.filter((todo) => todo.id !== id)
-  );
-};
 
-let upperCaseAll = () => {
-  setTodos((prevTodos) => {
-    return prevTodos.map((todo) => {
-      return {
-        ...todo,
-        task: todo.task.toUpperCase()
-      };
+  // Delete Task
+  let deleteTodo = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.filter((todo) => todo.id !== id)
+    );
+  };
+
+  // Uppercase All Tasks
+  let upperCaseAll = () => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        return {
+          ...todo,
+          task: todo.task.toUpperCase(),
+        };
+      });
     });
-  });
-};
+  };
+
+  // Uppercase One Task
+  let upperCaseOne = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            task: todo.task.toUpperCase(),
+          };
+        } else {
+          return todo;
+        }
+      });
+    });
+  };
+
+  // Mark Task Done
+  let taskDone = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            done: !todo.done,
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
 
   return (
     <div className="container">
@@ -44,18 +96,47 @@ let upperCaseAll = () => {
 
       <button onClick={addNewTask}>Add Task</button>
 
-      <h4>Task Todo</h4>
+      <h3>Task Todo</h3>
 
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            <span>{todo.task}</span>
-            &nbsp;&nbsp;&nbsp;
-            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            <span
+              style={{
+                textDecoration: todo.done
+                  ? "line-through"
+                  : "none",
+              }}
+            >
+              {todo.task}
+            </span>
+
+            &nbsp;&nbsp;
+
+            <input
+              type="checkbox"
+              checked={todo.done}
+              onChange={() => taskDone(todo.id)}
+            />
+
+            &nbsp;&nbsp;
+
+            <button onClick={() => deleteTodo(todo.id)}>
+              Delete
+            </button>
+
+            &nbsp;&nbsp;
+
+            <button onClick={() => upperCaseOne(todo.id)}>
+              UpperCase One
+            </button>
           </li>
         ))}
       </ul>
-        <button onClick={upperCaseAll}>Upper Case</button>
+
+      <button onClick={upperCaseAll}>
+        UpperCase All
+      </button>
     </div>
   );
 }
