59755cea92f3dda852fbdb63e85edd204128fe74
taslima864 <sheikhtaslima00@gmail.com>
2026-05-10 08:59:21 +0530

    Lottery Game!

--- React/react-state-class/src/Lottery.jsx
+++ React/react-state-class/src/Lottery.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import "./Lottery.css"
-import { genTicket } from "./helper";
+import { genTicket,sum } from "./helper";
 
 export default function Lottery() {
   let [ticket, setTicket] = useState(genTicket(3));
+  let isWinning = sum(ticket)  === 15;
+
+  let buyTicket = () => {
+    setTicket(genTicket(3));
+  }
   return (
     <div>
       <h1>Lottery Game! </h1>
@@ -12,6 +17,9 @@ export default function Lottery() {
         <span>{ticket[1]}</span>
         <span>{ticket[2]}</span>
       </div>
+      <button onClick={buyTicket}>Buy New Ticket</button>
+      <h3>{isWinning && "Congratulations, you won!"}</h3>
+
     </div>
   );
 }
